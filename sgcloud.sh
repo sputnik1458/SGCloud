5a53429cd72692786a44618fe56ee22ae05c1d5c
sputnik1458 <sputnik1@protonmail.com>
2016-10-07 23:41:15 -0400

    Update sgcloud.sh

--- sgcloud.sh
+++ sgcloud.sh
@@ -6,10 +6,5 @@ if [ ! -d ~/$CONFIG_DIR ]; then
     ./setup.sh
 fi
 
-#cd ~/$CONFIG_DIR/scripts
-#./pull.sh </dev/null &>/dev/null &
-#echo "Started remote sync"
-#./push.sh </dev/null &>/dev/null &
-#echo done
 ./sync.sh </dev/null &>/dev/null &
 echo "Sync started"
